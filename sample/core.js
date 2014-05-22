546ec7a4b5364c275a42b136185045bb5856e607
131e55 <131e550807@gmail.com>
2014-05-23 00:24:18 +0900

    Implement 'width' and 'height' properties

--- sample/core.js
+++ sample/core.js
@@ -4,11 +4,14 @@ window.onload = function () {
   var core = new Core(320, 320);
 
   core.onload = function () {
-    core.rootScene.backgroundColor = "#eee";
+    core.rootScene.backgroundColor = '#eee';
 
     var field = new TextField();
     field.x = 96;
     field.y = 32;
+    field.width = 160;
+    field.height = 32;
+    console.log(field);
     core.rootScene.addChild(field);
   };
 
--- textField.enchant.js
+++ textField.enchant.js
@@ -1,69 +1,31 @@
-enchant.TextField = enchant.Class.create(enchant.Node, {
-  initialize: function() {
-    enchant.Node.call(this);
+(function() {
+  enchant.TextField = enchant.Class.create(enchant.DomLayer, {
+    initialize: function() {
+      enchant.DomLayer.call(this);
 
-    // Remove 37 (left-key), 39 (right-key) and 32 (space-key)
-    // from 'enchant.ENV.PREVENT_DEFAULT_KEY_CODES'
-    this._setPreventDefaultKeyCodes();
+      // Remove 37 (left-key), 39 (right-key) and 32 (space-key)
+      // from 'enchant.ENV.PREVENT_DEFAULT_KEY_CODES'
+      this._setPreventDefaultKeyCodes();
 
-    // input element
-    this.input = document.createElement('input');
-    this.input.type = 'text';
-    this.input.style.position = 'absolute';
-    this.input.style.left = '0px';
-    this.input.style.top = '0px';
-    enchant.Core.instance._element.appendChild(this.input);
-  },
-
-  /*
-  * Append to enchant.Node#x
-  */
-  x: {
-    get: function() {
-      return this._x;
+      // input element
+      this.input = document.createElement('input');
+      this.input.type = 'text';
+      this.input.style.boxSizing = 'border-box';
+      this.input.style.width = '100%';
+      this.input.style.height = '100%';
+      this._element.appendChild(this.input);
     },
-    set: function(x) {
-      this._x = x;
-      this._dirty = true;
 
-      // Append from here
-      this._setPosition(this._x, this._y);
+    /*
+    * Remove 37 (left-key), 39 (right-key) and 32 (space-key)
+    * from 'enchant.ENV.PREVENT_DEFAULT_KEY_CODES'
+    */
+    _setPreventDefaultKeyCodes: function() {
+      var codes = enchant.ENV.PREVENT_DEFAULT_KEY_CODES;
+      codes = codes.filter(function (code) {
+        return (code !== 37 && code !== 39 && code !== 32);
+      });
+      enchant.ENV.PREVENT_DEFAULT_KEY_CODES = codes;
     }
-  },
-
-  /*
-  * Append to enchant.Node#y
-  */
-  y: {
-    get: function() {
-      return this._y;
-    },
-    set: function(y) {
-      this._y = y;
-      this._dirty = true;
-
-      // Append from here
-      this._setPosition(this._x, this._y);
-    }
-  },
-
-  /*
-  * Set input element position
-  */
-  _setPosition: function(x, y) {
-    this.input.style.left = this.x + 'px';
-    this.input.style.top = this.y + 'px';
-  },
-
-  /*
-  * Remove 37 (left-key), 39 (right-key) and 32 (space-key)
-  * from 'enchant.ENV.PREVENT_DEFAULT_KEY_CODES'
-  */
-  _setPreventDefaultKeyCodes: function() {
-    var codes = enchant.ENV.PREVENT_DEFAULT_KEY_CODES;
-    codes = codes.filter(function (code) {
-      return (code !== 37 && code !== 39 && code !== 32);
-    });
-    enchant.ENV.PREVENT_DEFAULT_KEY_CODES = codes;
-  }
-});
+  });
+})();
