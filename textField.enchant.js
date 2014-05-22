e457a492513d51eb6a0331af60267409ba4d5131
131e55 <131e550807@gmail.com>
2014-05-23 00:59:45 +0900

    Add blur input element, .focus() and .blur()

--- textField.enchant.js
+++ textField.enchant.js
@@ -2,6 +2,7 @@
   enchant.TextField = enchant.Class.create(enchant.DomLayer, {
     initialize: function() {
       enchant.DomLayer.call(this);
+      var that = this;
 
       // Remove 37 (left-key), 39 (right-key) and 32 (space-key)
       // from 'enchant.ENV.PREVENT_DEFAULT_KEY_CODES'
@@ -14,6 +15,27 @@
       this.input.style.width = '100%';
       this.input.style.height = '100%';
       this._element.appendChild(this.input);
+
+      // Blur input element, when scene touched (not input element)
+      this.on('addedtoscene', function () {
+        this.scene.on('touchstart', function () {
+          that.blur();
+        });
+      });
+    },
+
+    /*
+    * Focus input element
+    */
+    focus: function() {
+      this.input.focus();
+    },
+
+    /*
+    * Blur input element
+    */
+    blur: function() {
+      this.input.blur();
     },
 
     /*
